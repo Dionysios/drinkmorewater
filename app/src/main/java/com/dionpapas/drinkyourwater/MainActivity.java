ce0ad719c7ba627f513676af4f090f74c833cd8a
Dionysios Papathanopoulos <dpapathanopoulos@webst.gr>
2018-09-19 19:52:33 +0200

    Adding settings Activity

--- app/src/main/java/com/dionpapas/drinkyourwater/MainActivity.java
+++ app/src/main/java/com/dionpapas/drinkyourwater/MainActivity.java
@@ -1,7 +1,11 @@
 package com.dionpapas.drinkyourwater;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater =  getMenuInflater();
+        inflater.inflate(R.menu.settings_menu ,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if(id == R.id.action_settings){
+            Intent startSettingsActivity = new Intent(this, SettingsActivity.class);
+            startActivity(startSettingsActivity);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
