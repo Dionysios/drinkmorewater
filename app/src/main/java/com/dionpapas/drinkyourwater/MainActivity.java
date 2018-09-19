567ee7736f900cba819d26929961d378c342b6d2
Dionysios Papathanopoulos <dpapathanopoulos@webst.gr>
2018-09-19 21:16:29 +0200

    Finishing working with Preferences. Start work on the Notification

--- app/src/main/java/com/dionpapas/drinkyourwater/MainActivity.java
+++ app/src/main/java/com/dionpapas/drinkyourwater/MainActivity.java
@@ -1,18 +1,29 @@
 package com.dionpapas.drinkyourwater;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements SharedPreferences.OnSharedPreferenceChangeListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setupSharedPreferences();
+    }
+
+    private void setupSharedPreferences() {
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+        sharedPreferences.getBoolean(getString(R.string.enable_notif_key),
+                getResources().getBoolean(R.bool.pref_enable_notif));
+        sharedPreferences.registerOnSharedPreferenceChangeListener(this);
+
     }
 
     @Override
@@ -31,5 +42,20 @@ public class MainActivity extends AppCompatActivity {
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+        if(key.equals(getString(R.string.enable_notif_key))){
+            //todo Probably I need some action here
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        PreferenceManager.getDefaultSharedPreferences(this).
+                unregisterOnSharedPreferenceChangeListener(this);
+
     }
 }
